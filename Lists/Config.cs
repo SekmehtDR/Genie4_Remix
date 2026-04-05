e5edcb455bff88e45afb2dac48c881560bce6f52
SekmehtDR <50120656+SekmehtDR@users.noreply.github.com>
2026-04-05 09:56:37 -0700

    Disable check for updates at startup (To Original G4 Repo)

--- Lists/Config.cs
+++ Lists/Config.cs
@@ -64,9 +64,9 @@ namespace GenieClient.Genie
         public bool PromptBreak { get; set; } = true;
         public bool PromptForce { get; set; } = true;
         public bool Condensed { get; set; } = false;
-        public bool CheckForUpdates { get; set; } = true;
+        public bool CheckForUpdates { get; set; } = false;
         public bool AutoUpdate { get; set; } = false;
-        public bool AutoUpdateLamp { get; set; } = true;
+        public bool AutoUpdateLamp { get; set; } = false;
 
         public string sConnectString = "FE:GENIE /VERSION:" + My.MyProject.Application.Info.Version.ToString() + " /P:WIN_XP /XML";
         public int[] iPickerColors = new int[17];
